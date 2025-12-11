3ff7a6f9536b112521cc4cba58dfff989a70f7ab
lilToucan <ivancamuzzi@gmail.com>
2025-12-11 15:59:34 +0100

    ProjectStart

---
+++ Source/BuffMage/BuffMage.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BuffMage.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BuffMage, "BuffMage" );
