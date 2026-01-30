60c3951210e5987ada022a06ea3b58be1f5d1b3f
lilToucan <ivancamuzzi@gmail.com>
2026-01-30 11:10:42 +0100

    working on the WeaponData

--- Source/BuffMage/DataAssets/WeaponDataAsset.h
+++ Source/BuffMage/DataAssets/WeaponDataAsset.h
@@ -1,24 +1,29 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
 #include "WeaponDataAsset.generated.h"
 
-/**
- * 
- */
+
 UCLASS()
 class BUFFMAGE_API UWeaponDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
+
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
 	float Damage = 1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float Range = 400;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float PositionOffsetX = 400;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	float FireRate = 3;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack", meta= (ToolTip="the angle you want the player to check after performing an attack"))
+	float AngleDetection = 180;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Animation")
-	TObjectPtr<UAnimMontage> AttackComboAnimMontage;
+	TArray<TObjectPtr<UAnimMontage>> AttackComboAnimMontage;
 
-	
+	void Attack(FVector StartPos, TArray<TEnumAsByte<EObjectTypeQuery>>& CollisionChannel);
 };
