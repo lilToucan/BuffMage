1ee6ba93ac6619d0ceaad94cd8c2c4f10014383a
lilToucan <ivancamuzzi@gmail.com>
2026-01-31 18:44:41 +0100

    fixed cone issue

--- Source/BuffMage/DataAssets/Weapons/MeleeWeaponData.cpp
+++ Source/BuffMage/DataAssets/Weapons/MeleeWeaponData.cpp
@@ -10,38 +10,40 @@ void UMeleeWeaponData::Attack(FVector StartPos, TArray<TEnumAsByte<ECollisionCha
 
 	TArray<AActor*> IgnoreActors;
 	IgnoreActors.Add(Instigator);
-	
+
 	UClass* SeekClass = nullptr;
 	TArray<AActor*> OutActors;
 
-	DrawDebugSphere(Instigator->GetWorld(), StartPos,Range,12,FColor::Red,true,100.f,0,0);
-	UKismetSystemLibrary::SphereOverlapActors(Instigator->GetWorld(), StartPos, Range, traceObjectTypes, SeekClass, IgnoreActors, OutActors);
+	DrawDebugSphere(Instigator->GetWorld(), StartPos, Range, 12, FColor::Red, true, 100.f, 0, 0);
+	UKismetSystemLibrary::SphereOverlapActors(Instigator->GetWorld(), StartPos, Range, traceObjectTypes, SeekClass,
+	                                          IgnoreActors, OutActors);
 
 	if (OutActors.Num() < 1)
 		return;
-	
+
 	float ConeDot = -1; // set as the whole circle
 	if (AngleDetection != 360)
 	{
 		// Get the half Angle of the cone and transform it to Radiant
 		float RadAngle = (AngleDetection / 2) * (PI / 180);
-		
+
 		ConeDot = FMath::Cos(RadAngle); // get the Cone in dot product 
-		
 	}
-	
+
 	FDamageEvent DamageEvent;
 
 	for (AActor* Actor : OutActors)
 	{
 		if (AngleDetection != 360.f)
 		{
-			float Dot = FVector::DotProduct(Instigator->GetActorLocation(), Actor->GetActorLocation());
+			FVector Dir = Actor->GetActorLocation() - Instigator->GetActorLocation();
+			Dir.Normalize();
+			float Dot = FVector::DotProduct(Instigator->GetActorForwardVector(), Dir);
 
 			if (Dot < ConeDot) // 0.7f
 				continue;
 		}
-		
-		Actor->TakeDamage(Damage, DamageEvent,nullptr,Instigator);
+
+		Actor->TakeDamage(Damage, DamageEvent, nullptr, Instigator);
 	}
 }
