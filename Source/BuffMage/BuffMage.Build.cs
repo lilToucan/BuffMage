f1122c717eb8b8f7e51a338571ac6de3902c0e46
lilToucan <ivancamuzzi@gmail.com>
2026-03-15 19:59:19 +0100

    small fix with the build.cs

--- Source/BuffMage/BuffMage.Build.cs
+++ Source/BuffMage/BuffMage.Build.cs
@@ -10,10 +10,7 @@ public class BuffMage : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Slate","SlateCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {   "UnrealEd",
-			"PropertyEditor",
-			"EditorSubsystem",
-			"AudioPlatformConfiguration"});
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
